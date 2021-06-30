a45d9b7109a96286b1ab1ff7b5828b640b162f8f
AnjaliSureshKumar <anjalisureshkumar98@gmail.com>
2021-06-30 15:36:36 +0530

    all files added

---
+++ myechoserver.java
@@ -0,0 +1,22 @@
+import java.net.*;
+import java.io.*;
+class MyechoServer{
+public static void main(String args[])throws Exception{
+ServerSocket ss=new ServerSocket(4999);
+Socket s=ss.accept();
+DataInputStream din=new DataInputStream(s.getInputStream());
+DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+String str="",str2="";
+while(!str.equals("stop")){
+str=din.readUTF();
+System.out.println("client says: "+str);
+
+dout.writeUTF(str2);
+dout.flush();
+}
+din.close();
+s.close();
+ss.close();
+}}
